7b2e147184a29cd10d44380e7912aab59415c61e
Andrey Koloskov <andreykoloskov.ru@gmail.com>
2019-05-05 13:09:54 +0300

    add project4_1 HelloWorld

---
+++ build_visual_studio_2017_2019_project.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+echo "INIT MSVS2017_2019 Environment"
+cd "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\" 
+call vcvars64.bat
+cd %~dp0
+
+if not exist build_vs (
+    echo "CREATE build_vs directory"
+    md build_vs
+)
+
+cd build_vs
+
+echo "call cmake"
+cmake -G "Visual Studio 15 2017" ^
+	-A x64 ^
+	../
+
+cmake --build . -- -m:12
