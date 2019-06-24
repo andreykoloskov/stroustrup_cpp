a015e8d9c02145d1fa6d80daecaa61d42523b233
Andrey Koloskov <koloskov@alfadirect.ru>
2019-06-24 17:46:53 +0300

    operator

---
+++ src/project11_3/int.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Int
+{
+public:
+    Int(int x = 0): a(x) {}
+	Int(Int& x) { a = x.a; }
+	operator int() { return a; }
+	Int operator+(Int x) { Int i(a + x.a); return i; }
+
+private:
+    int a;
+};
+
+int main(int argc, char **argv)
+{
+	Int x = 2 + 3;
+	std::cout << (int) x << std::endl;
+    return 0;
+}
